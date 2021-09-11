d1c2854c79cdb2fbe02462ddd6b917ae410ad2fb
neil <neil@zhaninfo.com>
2021-09-11 09:37:26 +0800

    加入專案檔案。

---
+++ NHCS_110/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NHCS_110.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
